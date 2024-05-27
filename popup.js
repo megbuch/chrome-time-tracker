f60c5ae14d6309d13938b689e5733535f0d6d5ce
Meghan Bucher <115611931+megbuch@users.noreply.github.com>
2024-05-27 00:35:50 -0700

    Add button to clear all times

--- popup.js
+++ popup.js
@@ -1,9 +1,15 @@
 const startButton = document.getElementById('start-button')
 const pauseButton = document.getElementById('pause-button')
 const stopButton = document.getElementById('stop-button')
+const clearTimesButton = document.getElementById('clear-button')
 const timerDisplay = document.getElementById('timer-display')
 const timerLabelInput = document.getElementById('timer-name')
 const timesListElement = document.getElementById('times-list')
+
+startButton.addEventListener('click', startTimer)
+pauseButton.addEventListener('click', pauseTimer)
+stopButton.addEventListener('click', stopTimer)
+clearTimesButton.addEventListener('click', clearTimesFromStorage)
 
 let timerLabel = ''
 let startTime = 0
@@ -12,17 +18,10 @@ let intervalId = 0
 let isRunning = false
 let times = []
 
-chrome.storage.local.get('times', result => {
-  if (!result.times) return
-  times = result.times
-  times.forEach(timeItem => {
-    const li = document.createElement('li')
-    li.textContent = timeItem
-    timesListElement.appendChild(li)
-  });
-});
+// Immediately load list of times from local storage
+getTimesFromStorage()
 
-const startTimer = () => {
+function startTimer() {
   timerLabel = timerLabelInput.value
   startTime = Date.now() - elapsedTime
   intervalId = setInterval(updateTimer, 1000)
@@ -32,14 +31,14 @@ const startTimer = () => {
   stopButton.disabled = false
 }
 
-const pauseTimer = () => {
+function pauseTimer() {
   clearInterval(intervalId)
   isRunning = false
   startButton.disabled = false
   pauseButton.disabled = true
 }
 
-const stopTimer = () => {
+function stopTimer() {
   clearInterval(intervalId)
   isRunning = false
   elapsedTime = 0
@@ -49,28 +48,56 @@ const stopTimer = () => {
   saveTime()
 }
 
-const updateTimer = () => {
+function updateTimer() {
   elapsedTime = Date.now() - startTime
   const hours = Math.floor(elapsedTime / 3600000)
   const minutes = Math.floor((elapsedTime % 3600000) / 60000)
   const seconds = Math.floor((elapsedTime % 60000) / 1000)
   timerDisplay.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`
+
+  function pad(number) {
+    return number.toString().padStart(2, '0')
+  }
 }
 
-const saveTime = () => {
-  const timeItem = `${timerLabelInput.value}: ${timerDisplay.textContent}`
-  times.push(timeItem)
+function saveTime() {
+  // Set time in local storage
+  const time = {
+    label: timerLabelInput.value,
+    time: timerDisplay.textContent
+  }
+  times.push(time)
+  chrome.storage.local.set({ times })
+
+  // Add time to list
   const li = document.createElement('li')
-  li.textContent = timeItem
+  li.textContent = `${timerLabelInput.value} - ${timerDisplay.textContent}`
   timesListElement.appendChild(li)
+
+  // Reset
   timerDisplay.textContent = '00:00:00'
   timerLabelInput.value = ''
-  chrome.storage.local.set({ times })
 }
 
-startButton.addEventListener('click', startTimer)
-pauseButton.addEventListener('click', pauseTimer)
-stopButton.addEventListener('click', stopTimer)
+function getTimesFromStorage() {
+  chrome.storage.local.get('times', result => {
+    if (!result.times) return
+    times = result.times
+    
+    // Populate the list with times
+    times.forEach(time => {
+      const li = document.createElement('li')
+      li.textContent = `${time.label} ${time.time}`
+      timesListElement.appendChild(li)
+    });
+  });
+}
 
-// ---------- Helper Functions ----------
-const pad = number => number.toString().padStart(2, '0')
+function clearTimesFromStorage() {
+  chrome.storage.local.remove('times', () => {
+    times = []
+    while (timesListElement.firstChild) {
+      timesListElement.removeChild(timesListElement.firstChild)
+    }
+  })
+}
